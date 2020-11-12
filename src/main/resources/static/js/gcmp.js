0ded3e97c13cbf502a68e62466fe896b78e15a42
SZFHH <401952452@qq.com>
2020-11-12 17:01:14 +0800

    前端页面跳转

---
+++ src/main/resources/static/js/gcmp.js
@@ -0,0 +1,53 @@
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null)
+        return decodeURI(r[2]);
+    return null;
+}
+
+function handleError(err) {
+    if (err.response) {
+        var status = err.response.status;
+        if (status == 401) {
+            window.location.href = "/"
+        } else {
+            $("span.errorMessage").html(err.response.data.message);
+            $("div.ErrorMessageDiv").removeClass("fade")
+            window.setTimeout(function () {
+                $("div.ErrorMessageDiv").addClass("fade")
+            }, 2000)
+        }
+    }
+}
+
+function renderSize(value) {
+    if (null == value || value == '') {
+        return "0 Bytes";
+    }
+    var unitArr = new Array("Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB");
+    var index = 0;
+    var srcsize = parseFloat(value);
+    index = Math.floor(Math.log(srcsize) / Math.log(1024));
+    var size = srcsize / Math.pow(1024, index);
+    size = size.toFixed(2);//保留的小数位数
+    return size + unitArr[index];
+}
+
+function formatDate(date, fmt) {
+    var o = {
+        "M+": date.getMonth() + 1,                 //月份
+        "d+": date.getDate(),                    //日
+        "h+": date.getHours(),                   //小时
+        "m+": date.getMinutes(),                 //分
+        "s+": date.getSeconds(),                 //秒
+        "q+": Math.floor((date.getMonth() + 3) / 3), //季度
+        "S": date.getMilliseconds()             //毫秒
+    };
+    if (/(y+)/.test(fmt))
+        fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt))
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
